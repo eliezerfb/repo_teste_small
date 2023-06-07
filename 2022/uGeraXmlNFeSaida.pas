0f48338059a25c32586cbc05c7b1799f486de6cb
SandroSilvaSmallsoft <sandro@smallsoft.com.br>
2023-06-07 16:45:00 -0300

    Deixado comentado as linhas para adicionar inform. complementar do monofásico
    Identificado que a Tecnospeed deixou programado no DANFe para exibir automaticamente a mensagem de produto monofásico nas informações complementares. Foi comentado no código as linhas que adicionavam a mensagem, descomentar quando o DANFe for corrigido pela Tecnospeed

--- 2022/uGeraXmlNFeSaida.pas
+++ 2022/uGeraXmlNFeSaida.pas
@@ -2793,6 +2793,12 @@ begin
     Form7.spdNFeDataSets.Campo('infCpl_Z03').Value     := Trim(ConverteCaracterEspecialXML(Trim(Form7.ibDataSet15COMPLEMENTO.AsString + ' ' + Trim(sComplemento) + ' ' + sDIFAL_OBS + ' ' + sCupomReferenciado))); // Informacoes Complementares
   end;
 
+  {Sandro Silva 2023-06-07 inicio
+  // Ativar quando Tecnospeed deixar de incluir essa inform. complementar por conta própria na visualização do danfe
+  if vICMSMonoRet_N45Total > 0.00 then
+    Form7.spdNFeDataSets.Campo('infCpl_Z03').Value := Form7.spdNFeDataSets.Campo('infCpl_Z03').Value + '|' + 'ICMS monofásico sobre combustíveis cobrado anteriormente conforme Convênio ICMS 199/2022;';
+  {Sandro Silva 2023-06-07 fim}
+
   // Form7.spdNFeDataSets.Campo('xPed_ZB03').Value      := Form7.ibDataSet16NUMEROOS.AsString; // Informar o pedido no caso a OS
   // SAIDA
 end;
