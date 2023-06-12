1243af398da95e6491feb9dff0b7fdf43725c679
DailonParisottoSmall <131684502+DailonParisottoSmall@users.noreply.github.com>
2023-06-12 11:11:56 -0300

    Trocar caminho da unit
    - Trocar caminho

---
+++ uSmallConsts.pas
@@ -0,0 +1,35 @@
+unit uSmallConsts;
+
+interface
+
+const
+  _cSenhaSair = '#####';
+  _cUsuarioAdmin = 'Administrador';
+
+  _cSim = 'Sim';
+  _cNao = 'Não';
+  _cOKUpper  = 'OK';
+  _cOkCamel  = 'Ok';
+
+  //Incrementar a cada alteração do termo
+  _cVersaoTermoUso = 1;
+
+  // SECTION Geral (EST0QUE.DAT)
+  _cSectionGeral          = 'Geral';
+  _cIdentGeralRede        = 'Rede';
+  _cIdentGeralBuild       = 'Build';
+  _cIdentGeralBuild2020   = _cIdentGeralBuild + ' 2020';
+  _cIdentGeralBuild2023   = _cIdentGeralBuild + ' 2023';
+
+  // SECTION USO (smallcom.inf)
+  _cSectionUso = 'Uso';
+  _cIdentUso   = 'Uso';
+
+  // SECTION USUARIO ({USUARIO}.INF)
+  _cSectionGeralUsuario           = 'GERAL';
+  _cIdentGeralUsuMarketPlaceAtivo = 'MARKETPLACE ATIVO';
+  _cIdentGeralUsuMobileAtivo      = 'MOBILE ATIVO';
+
+implementation
+
+end.
