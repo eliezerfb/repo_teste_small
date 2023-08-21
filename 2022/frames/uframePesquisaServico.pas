c3ef477271d192ab9e9cdbe4ac24e1b741caa20c
DailonParisottoSmall <131684502+DailonParisottoSmall@users.noreply.github.com>
2023-08-21 16:59:43 -0300

    Ajustes teste
    - Ajustado grid piscando ao digitar
    - Ajustado para não diferencias caracteres especiais na pesquisa.

--- 2022/frames/uframePesquisaServico.pas
+++ 2022/frames/uframePesquisaServico.pas
@@ -27,26 +27,31 @@ procedure TframePesquisaServico.CarregarServico(AcPesquisar: String);
 begin
   if not Self.Visible then
     Exit;
-    
-  FqryRegistros.Close;
-  FqryRegistros.SQL.Clear;
-  FqryRegistros.SQL.Add('SELECT');
-  FqryRegistros.SQL.Add(' ESTOQUE.CODIGO');
-  FqryRegistros.SQL.Add(' , ESTOQUE.DESCRICAO AS DESCRICAO');
-  FqryRegistros.SQL.Add('FROM ESTOQUE');
-  FqryRegistros.SQL.Add('WHERE');
-  FqryRegistros.SQL.Add('(((COALESCE(ESTOQUE.ATIVO,0)=0) OR (COALESCE(ESTOQUE.ATIVO,0)=1)) AND (ESTOQUE.TIPO_ITEM='+QuotedStr('09')+'))');
-  if (LimpaNumero(AcPesquisar) = AcPesquisar) and (Length(LimpaNumero(AcPesquisar)) <= 5) then
-    FqryRegistros.SQL.Add('AND (ESTOQUE.CODIGO='+ QuotedStr(LimpaNumero(AcPesquisar))+')');
-  if (LimpaNumero(AcPesquisar) = AcPesquisar) and (Length(LimpaNumero(AcPesquisar)) > 5) then
-    FqryRegistros.SQL.Add('AND (ESTOQUE.REFERENCIA='+ QuotedStr(LimpaNumero(AcPesquisar))+')');
-  if (LimpaNumero(AcPesquisar) <> AcPesquisar) then
-    FqryRegistros.SQL.Add('AND (UPPER(ESTOQUE.DESCRICAO) LIKE ' + QuotedStr('%' + UpperCase(AcPesquisar) + '%') + ')');
-  FqryRegistros.SQL.Add('ORDER BY ESTOQUE.DESCRICAO');
-  FqryRegistros.Open;
-  FqryRegistros.FetchAll;
+  FqryRegistros.DisableControls;
+  try
+    FqryRegistros.Close;
+    FqryRegistros.SQL.Clear;
+    FqryRegistros.SQL.Add('SELECT');
+    FqryRegistros.SQL.Add(' ESTOQUE.CODIGO');
+    FqryRegistros.SQL.Add(' , ESTOQUE.DESCRICAO AS DESCRICAO');
+    FqryRegistros.SQL.Add('FROM ESTOQUE');
+    FqryRegistros.SQL.Add('WHERE');
+    FqryRegistros.SQL.Add('(((COALESCE(ESTOQUE.ATIVO,0)=0) OR (COALESCE(ESTOQUE.ATIVO,0)=1)) AND (ESTOQUE.TIPO_ITEM='+QuotedStr('09')+'))');
+    if (LimpaNumero(AcPesquisar) = AcPesquisar) and (Length(LimpaNumero(AcPesquisar)) <= 5) then
+      FqryRegistros.SQL.Add('AND (ESTOQUE.CODIGO='+ QuotedStr(LimpaNumero(AcPesquisar))+')');
+    if (LimpaNumero(AcPesquisar) = AcPesquisar) and (Length(LimpaNumero(AcPesquisar)) > 5) then
+      FqryRegistros.SQL.Add('AND (ESTOQUE.REFERENCIA='+ QuotedStr(LimpaNumero(AcPesquisar))+')');
+    if (LimpaNumero(AcPesquisar) <> AcPesquisar) then
+    begin
+      FqryRegistros.SQL.Add('AND ((UPPER(ESTOQUE.DESCRICAO) LIKE ' + QuotedStr('%') + '||UPPER(_iso8859_1 ' + QuotedStr(AcPesquisar) + ')||' + QuotedStr('%') + ')');
+      FqryRegistros.SQL.Add('OR (LOWER(ESTOQUE.DESCRICAO) LIKE ' + QuotedStr('%') + '||LOWER(_iso8859_1 ' + QuotedStr(AcPesquisar) + ')||' + QuotedStr('%') + '))');
+    end;
 
-  dbgItensPesq.Columns[Pred(dbgItensPesq.Columns.Count)].Width := dbgItensPesq.Width - 22;  
+    FqryRegistros.SQL.Add('ORDER BY ESTOQUE.DESCRICAO');
+    FqryRegistros.Open;
+  finally
+    FqryRegistros.EnableControls;
+  end;
 end;
 
 constructor TframePesquisaServico.Create(AOwner: TComponent);
