6b6de187186951079dbfb80a64f059ab2c35e736
DailonParisottoSmall <131684502+DailonParisottoSmall@users.noreply.github.com>
2023-09-15 10:16:22 -0300

    Ficha 7297
    - Arquivos restantes.

--- uSmallComINF.pas
+++ uSmallComINF.pas
@@ -3,17 +3,20 @@ unit uSmallComINF;
 interface
 
 uses
-  uArquivoDATINFPadrao, uSectionGeralDAT, uSectionUsoINF;
+  uArquivoDATINFPadrao, uSectionsSmallComINF;
 
 type
   TSmallComINF = class(TArquivoDATINFPadrao)
   private
     FoUso: TSectionUso;
+    FoOutros: TSectionOutros;
     function getUso: TSectionUso;
+    function getOutros: TSectionOutros;
   public
     destructor Destroy; override;
 
     property Uso: TSectionUso read getUso;
+    property Outros: TSectionOutros read getOutros;
   protected
     function NomeArquivo: String; override;
   end;
@@ -28,8 +31,18 @@ destructor TSmallComINF.Destroy;
 begin
   if Assigned(FoUso) then
     FreeAndNil(FoUso);
+  if Assigned(FoOutros) then
+    FreeAndNil(FoOutros);
 
   inherited;
+end;
+
+function TSmallComINF.getOutros: TSectionOutros;
+begin
+  if not Assigned(FoOutros) then
+    FoOutros := TSectionOutros.Create(FoIni);
+
+  Result := FoOutros;
 end;
 
 function TSmallComINF.getUso: TSectionUso;
