fe19eafdaef1a1d5821074a7bfabd620b2fb35e1
SandroSilvaSmallsoft <sandro@smallsoft.com.br>
2024-01-05 15:25:31 -0300

    Ficha 7750 - Outros - Migração Delphi - 6 - Telas de edição de cadastros estão cortando a direita, do lado do botão OK
    Ajustado a altura do form

--- 2022/Unit10.pas
+++ 2022/Unit10.pas
@@ -4426,7 +4426,7 @@ begin
   Orelhas.ActivePage := Orelha_cadastro;
 
   Form10.Width  := 855; // 845;
-  Form10.Height := 650;
+  Form10.Height := 655; // 650;
 
   btnOK.Left  := Panel2.Width - btnOK.Width - 10;
   btnRenogiarDivida.Left  := btnOK.Left - 10 - btnRenogiarDivida.Width;
