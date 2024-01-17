03a943e597d749a42fc2cbb8d65e6943c47ae1de
Sandro Silva <72104567+smallsoft-sandro@users.noreply.github.com>
2024-01-17 11:42:28 -0300

    Update uEmail.pas

--- uEmail.pas
+++ uEmail.pas
@@ -6,6 +6,8 @@ function ValidaEmail(email: String): Boolean;
 
 implementation
 
+uses uITestaEmail, uTestaEmail;
+
 function ValidaEmail(email: String): Boolean;
 begin
   Result := TTestaEmail.New.setEmail(email).Testar;
