6fe07812babb31ee3e867bbfbb96a1b4081db752
Sandro Silva <72104567+smallsoft-sandro@users.noreply.github.com>
2024-08-15 21:30:50 -0300

    Definido build 2024.28.0.2

--- 2022/user.dpr
+++ 2022/user.dpr
@@ -4,7 +4,12 @@ uses
   Forms,
   user1 in 'user1.pas' {Form1},
   uTestaEmail in '..\..\unit_compartilhada\uTestaEmail.pas',
-  uITestaEmail in '..\..\unit_compartilhada\interfaces\uITestaEmail.pas';
+  uITestaEmail in '..\..\unit_compartilhada\interfaces\uITestaEmail.pas',
+  smallfunc_xe in '..\..\unit_compartilhada\smallfunc_xe.pas',
+  uConectaBancoSmall in '..\..\unit_compartilhada\uConectaBancoSmall.pas',
+  uDialogs in '..\..\unit_compartilhada\uDialogs.pas',
+  uconstantes_chaves_privadas in '..\..\..\uconstantes_chaves_privadas.pas',
+  uSmallConsts in '..\..\unit_compartilhada\uSmallConsts.pas';
 
 {$R *.RES}
 
