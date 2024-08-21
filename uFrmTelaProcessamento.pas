0a5b45c9ae7afb46e1451bb788b1e9d1d164d11c
Dailon Parisotto <dailon.parisotto@smallsoft.com.br>
2024-08-21 08:40:07 -0300

    Clean code
    Removido uses que não era utilizada na unit.

--- uFrmTelaProcessamento.pas
+++ uFrmTelaProcessamento.pas
@@ -5,7 +5,7 @@ interface
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.ExtCtrls, vcl.Imaging.GIFImg,
-  Vcl.ComCtrls, frxGIFGraphic;
+  Vcl.ComCtrls;
 
   procedure MostraTelaProcessamento(sTexto:string);
   procedure FechaTelaProcessamento();
